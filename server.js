65081fb115e48e96378e002395b861946f229bd6
malhill <74620676+malhill@users.noreply.github.com>
2021-03-23 22:05:43 -0400

    pseudo code in server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// each one will become more specific 
+// (skateboard) general genre playlist
+// (motorbike) spcific playlist
+// (car) specific
+// (rocket ship) adding specific songs adding to a new playlist
+
+// Surprise 
+// Fear
+// Disgust
+// Contempt
+// Anger
+// Sadness
+// Happiness
+// neutral 
+
+// Maybe create a html file with buttons with the major emotions
+// have buttons create song(s) from api(s)
+// populate onto page
+// progress if possible
